628935d7ae58217dbf1fa31591812ea02b0bd3aa
Jaxon Petersen <thatCashBar@gmail.com>
2021-03-04 15:50:53 -0500

    Refactored functions and added CSS styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
@@ -7,69 +6,54 @@ const el = React.createElement;
 class App extends React.Component {
   state = {
     total: 0,
-    inputValue: 0 
+    inputValue: 0
   }
 
   handleInputChange = (event) => {
     this.setState({
-      inputValue: event.target.value
+      inputValue: event.target.value,
     })
   }
 
-  calculate = () => {
-
+  resetHandler = () => {
+    this.setState({
+      total: 0,
+      inputValue: 0
+    })
   }
- 
-  add = () => {
-    let result = Number(this.state.total) + Number(this.state.inputValue);
 
-    console.log("Total: ", result);
-    console.log("Val: ", result);
+  calculate = (incomingEvent) => {
+    let result;
+    switch (incomingEvent.target.name) 
+    {
+      case "add":
+        result = Number(this.state.total) + Number(this.state.inputValue);
+        break;
+      case "subtract":
+        result = Number(this.state.total) - Number(this.state.inputValue);
+        break;
+      case "divide":
+        result = Number(this.state.total) / Number(this.state.inputValue);
+        break;
+      case "multiply":
+        result = Number(this.state.total) * Number(this.state.inputValue);
+        break;
+    }
     this.setState({
       total: [result]
     }); 
   }
-
-  subtract = () => {
-    let result = Number(this.state.total) - Number(this.state.inputValue);
-
-    console.log("Total: ", result);
-    console.log("Val: ", result);
-    this.setState({
-      total: [result]
-    }); 
-  }
-
-  divide = () => {
-    let result = Number(this.state.total) / Number(this.state.inputValue);
-
-    console.log("Total: ", result);
-    console.log("Val: ", result);
-    this.setState({
-      total: [result]
-    }); 
-  }
-
-  multiply = () => {
-    let result = Number(this.state.total) * Number(this.state.inputValue);
-
-    console.log("Total: ", result);
-    console.log("Val: ", result);
-    this.setState({
-      total: [result]
-    }); 
-  }
-
 
   render() {
-    return el("form", {id: "calcForm"}, 
-      el("p", {name: "result"}, this.state.total),
-      el("input", {name: "inputValue", placeholder: "input", onChange: this.handleInputChange}, null),
-      el("div", null,
-        el("button", {name: "add", type:"button", onClick: this.add}, "Add"),
-        el("button", {name: "subtract", type:"button", onClick: this.subtract}, "Subtract"),
-        el("button", {name: "multiply", type:"button", onClick: this.multiply}, "Multiply"),
-        el("button", {name: "divide", type:"button", onClick: this.divide}, "Divide"),
+    return el("form", {id: "calcForm", onReset: this.resetHandler}, 
+      el("p", {name: "result"}, `Total: ${this.state.total}`),
+      el("input", {name: "inputValue", placeholder: "Input", onChange: this.handleInputChange}, null),
+      el("button", {name: "reset", type:"reset"}, "Reset"),
+      el("div", { id: "buttonContainer"},
+        el("button", {class: "opButton", name: "add", type:"button", onClick: this.calculate}, "Add"),
+        el("button", {class: "opButton", name: "subtract", type:"button", onClick: this.calculate}, "Subtract"),
+        el("button", {class: "opButton", name: "multiply", type:"button", onClick: this.calculate}, "Multiply"),
+        el("button", {class: "opButton", name: "divide", type:"button", onClick: this.calculate}, "Divide")
       )
     );
   }
